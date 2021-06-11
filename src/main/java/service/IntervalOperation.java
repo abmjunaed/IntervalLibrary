736a8695c0f5580b69d788850a5524cee714d140
test <test@test.com>
2021-06-11 22:36:39 +0200

    -Added the implementation and the tests

---
+++ src/main/java/service/IntervalOperation.java
@@ -0,0 +1,46 @@
+package service;
+
+import model.Interval;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class IntervalOperation {
+    /**
+     * This function doesn't return {{@code null}}; it returns empty list if the input is empty or {{@code null}}.
+     * This function merges the overlapping intervals and returns the disjoint intervals.
+     * e.g, for the input  [[25,30], [2,19], [14, 23], [4,8]], it returns [[2,23], [25,30]]
+     *
+     * Algorithm: sort the intervals based on the first value. Then check the overlapping by comparing the start of an interval with the previous interval's end.
+     * Increase the end of an interval by taking the bigger of the end of the two overlapping intervals.
+     */
+    public List<Interval> mergeOverlappedIntervals(List<Interval> intervals) {
+
+        if (intervals == null || intervals.isEmpty()) return new ArrayList<>();
+
+        sortByStart(intervals);
+
+        List<Interval> result = new ArrayList<>();
+
+        int start = intervals.get(0).getStart();
+        int end = intervals.get(0).getEnd();
+
+        for (int i = 1; i < intervals.size(); i++) {
+            Interval currentInterval=intervals.get(i);
+
+            if (currentInterval.getStart() > end) { //outside the last interval
+                result.add(new Interval(start, end)); //found the previous disjoint set
+                start = currentInterval.getStart(); //start of new disjoint set
+            }
+            end = Math.max(currentInterval.getEnd(), end); //update the end of the disjoint set
+        }
+        result.add(new Interval(start, end));
+
+        return result;
+    }
+
+    private void sortByStart(List<Interval> intervals) {
+        intervals.sort(Comparator.comparingInt(Interval::getStart));
+    }
+}
---
+++ src/test/java/service/IntervalOperationTest.java
@@ -0,0 +1,73 @@
+package service;
+
+import model.Interval;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class IntervalOperationTest {
+    private final IntervalOperation operation = new IntervalOperation();
+
+    @Test
+    public void mergeWithEmptyList() {
+        assertTrue(operation.mergeOverlappedIntervals(new ArrayList<>()).isEmpty());
+    }
+
+    @Test
+    public void mergeWithNullList() {
+        assertTrue(operation.mergeOverlappedIntervals(null).isEmpty());
+    }
+
+    @Test
+    public void mergeWithOneList() {
+        List<Interval> oneElement = Stream.of(new Interval(1, 9)).collect(Collectors.toList());
+        List<Interval> result = operation.mergeOverlappedIntervals(oneElement);
+        assertEquals(1, result.size());
+        assertEquals(1, result.get(0).getStart());
+        assertEquals(9, result.get(0).getEnd());
+    }
+
+    @Test
+    public void mergeUnsorted() {
+        List<Interval> input = Stream.of(new Interval(25, 30), new Interval(2, 19), new Interval(14, 23), new Interval(4, 8)).collect(Collectors.toList());
+        List<Interval> result = operation.mergeOverlappedIntervals(input);
+        assertEquals(2, result.size());
+        assertEquals(2, result.get(0).getStart());
+        assertEquals(23, result.get(0).getEnd());
+
+        assertEquals(25, result.get(1).getStart());
+        assertEquals(30, result.get(1).getEnd());
+    }
+
+    @Test
+    public void mergeWithStrictlySorted() {
+        List<Interval> oneElement = Stream.of(new Interval(1, 3), new Interval(1, 4), new Interval(6, 9)).collect(Collectors.toList());
+        List<Interval> result = operation.mergeOverlappedIntervals(oneElement);
+        assertEquals(2, result.size());
+        assertEquals(1, result.get(0).getStart());
+        assertEquals(4, result.get(0).getEnd());
+
+        assertEquals(6, result.get(1).getStart());
+        assertEquals(9, result.get(1).getEnd());
+    }
+
+    @Test
+    public void mergeWithFirstSortedSecondUnsorted() {
+        List<Interval> oneElement = Stream.of(new Interval(1, 4), new Interval(1, 3), new Interval(6, 9)).collect(Collectors.toList());
+        List<Interval> result = operation.mergeOverlappedIntervals(oneElement);
+        assertEquals(2, result.size());
+        assertEquals(1, result.get(0).getStart());
+        assertEquals(4, result.get(0).getEnd());
+
+        assertEquals(6, result.get(1).getStart());
+        assertEquals(9, result.get(1).getEnd());
+    }
+
+
+}
